398142508598cfceb75bf0c560c28ab3bed1bc17
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-30 21:40:03 +0500

    123

--- CleanCode&CodeStyle6/Program.cs
+++ CleanCode&CodeStyle6/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanCode_CodeStyle6
 {
-    internal class Program
+    internal class Programm
     {
-        static void Main(string[] args)
+        public static void CreateNewObject()
         {
+            //Создание объекта на карте
+        }
+
+        public static void GenerateChance()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int GetCalculatedSalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
         }
     }
 }
